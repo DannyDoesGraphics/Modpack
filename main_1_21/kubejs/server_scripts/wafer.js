474da224580b7b13db52308cb7d4b41795782e8e
DannyDoesGraphics <git@dannydoes.graphics>
2025-11-26 15:48:48 -0500

    did stuff

--- main_1_21/kubejs/server_scripts/wafer.js
+++ main_1_21/kubejs/server_scripts/wafer.js
@@ -1,19 +1,57 @@
 ServerEvents.recipes(event => {
 
-    // get raw silicon wafers
+    // Pull wafers from refined silicon ingots (press -> slice -> lap)
     event.custom({
-        type: "create:cutting",
-        ingredients: [
+        type: "create:sequenced_assembly",
+        ingredient: { item: "tfmg:silicon_ingot" },
+        transitional_item: { id: "dam:raw_silicon_wafer" },
+        loops: 1,
+        sequence: [
             {
-                item: "minecraft:quartz_block"
+                type: "create:pressing",
+                ingredients: [
+                    { item: "tfmg:silicon_ingot" }
+                ],
+                results: [
+                    { id: "dam:raw_silicon_wafer" }
+                ]
+            },
+            {
+                type: "create:cutting",
+                ingredients: [
+                    { item: "dam:raw_silicon_wafer" }
+                ],
+                results: [
+                    { id: "dam:raw_silicon_wafer" }
+                ],
+                processing_time: 100
+            },
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:raw_silicon_wafer" },
+                    { tag: "create:sandpaper" },
+                ],
+                keepHeldItem: true,
+                results: [
+                    { id: "dam:raw_silicon_wafer" }
+                ]
+            },
+            {
+                type: "create:filling",
+                ingredients: [
+                    { item: "dam:raw_silicon_wafer" },
+                    { type: "fluid_stack", amount: 250, fluid: "minecraft:water" }
+                ],
+                results: [
+                    { id: "dam:raw_silicon_wafer" }
+                ]
             }
         ],
         results: [
-            { id: "dam:raw_silicon_wafer", count: 4 },
-            { id: "dam:raw_silicon_wafer", chance: 0.25 },
-            { id: "minecraft:quartz", chance: 0.25 }
-        ],
-        processing_time: 200
+            { id: "dam:raw_silicon_wafer", count: 2 },
+            { id: "minecraft:cobblestone", count: 1 }
+        ]
     });
 
     // Rinse + polish 5x -> Incomplete wafer
@@ -50,7 +88,7 @@ ServerEvents.recipes(event => {
         ]
     });
 
-    // Clean -> Mask (kept) -> Etch in sulfuric acid -> Interconnect -> CMP/Anneal Proxy (8x)
+    // 6-step FEOL loop: Clean -> Oxidize -> Photoresist -> Etch -> Dope -> Anneal (8x)
     event.custom({
         type: "create:sequenced_assembly",
         ingredient: { item: "dam:incomplete_silicon_wafer" },
@@ -68,34 +106,26 @@ ServerEvents.recipes(event => {
                     { id: "dam:incomplete_silicon_wafer" }
                 ]
             },
-            // Deploy semiconductors (etching)
+            // Grow oxide barrier
             {
                 type: "create:deploying",
                 ingredients: [
                     { item: "dam:incomplete_silicon_wafer" },
-                    { item: "tfmg:p_semiconductor" }
+                    { item: "minecraft:quartz" }
                 ],
+                keepHeldItem: true,
                 results: [
                     { id: "dam:incomplete_silicon_wafer" }
                 ]
             },
-            {
-                type: "create:deploying",
-                ingredients: [
-                    { item: "dam:incomplete_silicon_wafer" },
-                    { item: "tfmg:n_semiconductor" }
-                ],
-                results: [
-                    { id: "dam:incomplete_silicon_wafer" }
-                ]
-            },
-            // Deploy with incomplete wafer + glass pane
+            // Spin on photoresist + mask stack
             {
                 type: "create:deploying",
                 ingredients: [
                     { item: "dam:incomplete_silicon_wafer" },
                     { item: "minecraft:glass_pane" }
                 ],
+                keepHeldItem: true,
                 results: [
                     { id: "dam:incomplete_silicon_wafer" }
                 ]
@@ -111,18 +141,19 @@ ServerEvents.recipes(event => {
                     { id: "dam:incomplete_silicon_wafer" }
                 ]
             },
-            // Interconnect deploy using copper sheet
+            // Dope exposed channels
             {
                 type: "create:deploying",
                 ingredients: [
                     { item: "dam:incomplete_silicon_wafer" },
-                    { item: "create:copper_sheet" }
+                    { item: "tfmg:transistor_item" }
                 ],
+                keepHeldItem: false,
                 results: [
                     { id: "dam:incomplete_silicon_wafer" }
                 ]
             },
-            // final pressing step
+            // Rapid thermal anneal / CMP proxy
             {
                 type: "create:pressing",
                 ingredients: [
@@ -134,9 +165,193 @@ ServerEvents.recipes(event => {
             }
         ],
         results: [
-            { id: "dam:silicon_wafer" }
+            { id: "dam:patterned_wafer" },
+            { id: "dam:defective_wafer", chance: 0.12 }
         ],
     })
+
+    // FEOL rework loop for failed wafers
+    event.custom({
+        type: "create:sequenced_assembly",
+        ingredient: { item: "dam:defective_wafer" },
+        transitional_item: { id: "dam:defective_wafer" },
+        loops: 4,
+        sequence: [
+            {
+                type: "create:filling",
+                ingredients: [
+                    { item: "dam:defective_wafer" },
+                    { type: "fluid_stack", amount: 250, fluid: "minecraft:water" }
+                ],
+                results: [
+                    { id: "dam:defective_wafer" }
+                ]
+            },
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:defective_wafer" },
+                    { tag: "create:sandpaper" }
+                ],
+                keepHeldItem: true,
+                results: [
+                    { id: "dam:defective_wafer" }
+                ]
+            },
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:defective_wafer" },
+                    { item: "minecraft:glass_pane" }
+                ],
+                keepHeldItem: true,
+                results: [
+                    { id: "dam:defective_wafer" }
+                ]
+            },
+            {
+                type: "create:filling",
+                ingredients: [
+                    { item: "dam:defective_wafer" },
+                    { type: "fluid_stack", amount: 100, fluid: "tfmg:sulfuric_acid" }
+                ],
+                results: [
+                    { id: "dam:defective_wafer" }
+                ]
+            }
+        ],
+        results: [
+            { id: "dam:patterned_wafer" }
+        ]
+    });
+
+    // Back-end metallization: dielectric -> pattern -> copper -> CMP (4x)
+    event.custom({
+        type: "create:sequenced_assembly",
+        ingredient: { item: "dam:patterned_wafer" },
+        transitional_item: { id: "dam:patterned_wafer" },
+        loops: 4,
+        sequence: [
+            // Deposit interlayer dielectric
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:patterned_wafer" },
+                    { item: "create:sturdy_sheet" }
+                ],
+                keepHeldItem: false,
+                results: [
+                    { id: "dam:patterned_wafer" }
+                ]
+            },
+            // Pattern trenches
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:patterned_wafer" },
+                    { item: "minecraft:redstone" }
+                ],
+                keepHeldItem: true,
+                results: [
+                    { id: "dam:patterned_wafer" }
+                ]
+            },
+            // Fill with copper
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:patterned_wafer" },
+                    { item: "create:copper_sheet" }
+                ],
+                keepHeldItem: false,
+                results: [
+                    { id: "dam:patterned_wafer" }
+                ]
+            },
+            // CMP to planarize
+            {
+                type: "create:pressing",
+                ingredients: [
+                    { item: "dam:patterned_wafer" }
+                ],
+                results: [
+                    { id: "dam:patterned_wafer" }
+                ]
+            }
+        ],
+        results: [
+            { id: "dam:metallized_wafer" },
+            { id: "dam:defective_metallized_wafer" }
+        ]
+    });
+
+    // BEOL rework loop for metal defects
+    event.custom({
+        type: "create:sequenced_assembly",
+        ingredient: { item: "dam:defective_metallized_wafer" },
+        transitional_item: { id: "dam:defective_metallized_wafer" },
+        loops: 2,
+        sequence: [
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:defective_metallized_wafer" },
+                    { item: "create:copper_sheet" }
+                ],
+                keepHeldItem: false,
+                results: [
+                    { id: "dam:defective_metallized_wafer" }
+                ]
+            },
+            {
+                type: "create:pressing",
+                ingredients: [
+                    { item: "dam:defective_metallized_wafer" }
+                ],
+                results: [
+                    { id: "dam:defective_metallized_wafer" }
+                ]
+            }
+        ],
+        results: [
+            { id: "dam:metallized_wafer" },
+            { id: "dam:defective_metallized_wafer", chance: 0.08 }
+        ]
+    });
+
+    // Passivation + electrical test -> finished wafer
+    event.custom({
+        type: "create:sequenced_assembly",
+        ingredient: { item: "dam:metallized_wafer" },
+        transitional_item: { id: "dam:metallized_wafer" },
+        loops: 2,
+        sequence: [
+            {
+                type: "create:filling",
+                ingredients: [
+                    { item: "dam:metallized_wafer" },
+                    { type: "fluid_stack", amount: 50, fluid: "minecraft:water" }
+                ],
+                results: [
+                    { id: "dam:metallized_wafer" }
+                ]
+            },
+            {
+                type: "create:deploying",
+                ingredients: [
+                    { item: "dam:metallized_wafer" },
+                    { item: "create:precision_mechanism" }
+                ],
+                keepHeldItem: true,
+                results: [
+                    { id: "dam:metallized_wafer" }
+                ]
+            }
+        ],
+        results: [
+            { id: "dam:silicon_wafer" }
+        ]
+    });
 
     // Cut the completed wafer
     event.custom({
